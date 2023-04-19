9bdf23688cf7304b2c149a6c9081af63825e7d44
Batudo <batudo@2pi.network>
2023-04-19 15:32:09 -0300

    Gatsby 5.9 + offline plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,12 +2,18 @@ import type { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
   graphqlTypegen: true,
-  plugins: ['gatsby-plugin-sass', 'gatsby-plugin-sitemap', {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      icon: 'src/images/logo.svg'
-    }
-  }],
+  plugins: [
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/logo.svg'
+      }
+    },
+    // Important: this must be after the manifest plugin
+    'gatsby-plugin-offline'
+  ],
   siteMetadata: {
     title:   `dCustody`,
     siteUrl: `https://dcustody.xyz`
